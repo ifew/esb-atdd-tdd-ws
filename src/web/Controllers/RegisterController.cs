b1747a98c7f160e68c79f398f29ab2d5d16b4ac5
iFew <chitpong@gmail.com>
2018-01-18 15:59:37 +0700

    Complete Code

---
+++ src/web/Controllers/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using web.Models;
+
+namespace web.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Confirmation(RegisterModel registerModel)
+        {
+            ViewData["registerModel.txt_CardNo"] = registerModel.txt_CardNo;
+            ViewData["registerModel.txt_CardID"] = registerModel.txt_CardID;
+            ViewData["registerModel.txt_BirthDate"] = registerModel.txt_BirthDate;
+            ViewData["registerModel.txt_MobileNo"] = registerModel.txt_MobileNo;
+            return View();
+        }
+
+        public IActionResult Finish()
+        {
+            return View();
+        }
+
+    }
+}
